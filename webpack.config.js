1fbee0743286f4c09648909ba9f8252d27c23078
Nono Martínez Alonso <mundowarezweb@gmail.com>
2019-07-09 16:30:57 +0200

    Add React.

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,26 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.ts',
-  devtool: 'inline-source-map',
+
+  mode: 'production',
+
+  entry: './src/index.tsx',
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: 'source-map',
   module: {
     rules: [
-        {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/
-        }
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }      
     ]
   },
   resolve: {
@@ -18,5 +29,13 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
   }
 };
